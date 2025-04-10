d6739b2b2cf4b3900cbe3526546d176f6ca81ac1
hansen1101 <hansen1101@gmail.com>
2025-04-10 20:15:15 +0200

    add harpoon settings

--- nvim/lua/core/plugins/harpoon.lua
+++ nvim/lua/core/plugins/harpoon.lua
@@ -7,9 +7,19 @@ return {
       local harpoon = require("harpoon")
       local extensions = require("harpoon.extensions");
 
-      harpoon:setup({})
+      harpoon:setup({
+        settings = {
+            save_on_toggle = true,
+            sync_on_ui_close = true,
+            key = function()
+                return vim.loop.cwd()
+                --return "static"
+            end,
+        },
+      })
       --harpoon:extend(extensions.builtins.command_on_nav("foo bar"));
       harpoon:extend(extensions.builtins.navigate_with_number());
+      harpoon:extend(extensions.builtins.highlight_current_file());
     end,
     config = function()
       local harpoon = require("harpoon")
