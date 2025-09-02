6fc3e1ef3e9ba7d1f7e8ba03524b7e48fa139420
hansen1101 <hansen1101@gmail.com>
2025-09-02 22:00:04 +0200

    neorg setup

--- nvim/lua/core/plugins/cmp.lua
+++ nvim/lua/core/plugins/cmp.lua
@@ -30,6 +30,7 @@ return {
         },
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
+          { name = "neorg" },
           -- { name = 'vsnip' }, -- For vsnip users.
           { name = 'luasnip' }, -- For luasnip users.
           -- { name = 'ultisnips' }, -- For ultisnips users.
---
+++ nvim/lua/core/plugins/neorg.lua
@@ -0,0 +1,61 @@
+return {
+  {
+      "nvim-neorg/neorg",
+      lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+      version = "*", -- Pin Neorg to the latest stable release
+      build = ":Neorg sync-parsers",
+--      config = true,
+      config = function()
+        require("neorg").setup({
+          load = {
+            ["core.summary"] = {},
+            ["core.highlights"] = {},
+            ["core.integrations.treesitter"] = {},
+            ["core.defaults"] = {},
+            ["core.export"] = {},    -- Enable export module
+            ["core.export.markdown"] = {},  -- Enable Markdown export
+            ["core.concealer"] = {},
+            ["core.dirman"] = {
+              config = {
+                workspaces = {
+                  notes = "~/workspace/github.com/tljoh/notes",
+                },
+                default_workspace = "notes",
+              }
+            },
+            --["core.concealer"] = {
+            --  config = {
+            --    folds = true,
+            --    icon_present = "diamond",
+            --  }
+            --},
+            ["core.completion"] = {
+              config = {
+                engine = "nvim-cmp"
+              }
+            },
+          },
+        })
+      end,
+      dependencies = {
+        { "nvim-lua/plenary.nvim", },
+        { "nvim-neorg/neorg-telescope" },
+        {
+            -- YOU ALMOST CERTAINLY WANT A MORE ROBUST nvim-treesitter SETUP
+            -- see https://github.com/nvim-treesitter/nvim-treesitter
+            "nvim-treesitter/nvim-treesitter",
+            opts = {
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            },
+            config = function(_,opts)
+                require('nvim-treesitter.configs').setup(opts)
+            end
+        },
+        --{ "folke/tokyonight.nvim", config=function(_,_) vim.cmd.colorscheme "tokyonight-storm" end,},
+    },
+  }
+}
