1abbaf929913e33a1be03cb844fb14e0244dde57
hansen1101 <hansen1101@gmail.com>
2025-09-26 13:31:23 +0200

    revert lua_ls config

--- nvim/lua/core/config/lsp.lua
+++ nvim/lua/core/config/lsp.lua
@@ -1,15 +1,1 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-vim.lsp.config.lua_ls = {
-  ok_attach = on_attach,
-  capabilities = capkbilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-    },
-  }
-}
-
 vim.lsp.enable('lua_ls')
